6d5f206a5715c912ade2b62d1990da3116f1c6c1
雷文锋 <leiwenfeng@skyinfor.com>
2021-01-12 13:42:12 +0800

    添加全局跨域中间件

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        //当前请求客户端
+        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+        //允许跨域客户端
+        $allow_origin = [
+            'http://localhost:8081',
+            'http://localhost:8082',
+            'http://localhost:8083',
+            'http://localhost:8080',
+        ];
+        //可在.env配置参数来控制是否需要验证跨域
+        if(!in_array($origin,$allow_origin))
+            return $response;
+
+        //通过跨域设置请求头
+        $headers = [
+            'Access-Control-Allow-Origin' => $origin,
+            'Access-Control-Allow-Headers' => 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN',
+            'Access-Control-Expose-Headers' => 'Authorization, authenticated',
+            'Access-Control-Allow-Methods' => 'GET, POST, PATCH, PUT, OPTIONS',
+            'Access-Control-Allow-Credentials' => 'true',
+        ];
+        $IlluminateResponse = 'Illuminate\Http\Response';
+        $SymfonyResopnse = 'Symfony\Component\HttpFoundation\Response';
+        // 因为 response 可能是两个不同的类 设置header 方式不一样
+        if ($response instanceof $IlluminateResponse) {
+            foreach ($headers as $key => $value) {
+                $response->header($key, $value);
+            }
+            return $response;
+        }
+        if ($response instanceof $SymfonyResopnse) {
+            foreach ($headers as $key => $value) {
+                $response->headers->set($key, $value);
+            }
+            return $response;
+        }
+    }
+}
