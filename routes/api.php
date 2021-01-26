06815c9b2834f9005a5232e3f254c7488edee3c7
leiwenfeng <lei809721586@qq.com>
2021-01-26 16:18:38 +0800

    按模块进行路由分组

--- routes/api.php
+++ routes/api.php
@@ -24,29 +24,42 @@ Route::namespace('Api')->prefix('v1')->middleware('apilog')->group(function () {
     /** 发送短信验证码 **/
     Route::post('/sendsms','SmsController@sendSms');
 
-    /** 微信小程序一键登录获取手机号码 */
-    Route::post('/user/weixin_login','UserController@weixLogin');
-
-    /** 获取微信用户openid */
-    Route::post('/user/get_openid','UserController@getOpenId');
-
-    /** vip卡列表 */
-    Route::get('/user/vip_card_list','UserController@vipCardList');
-
     /** 微信小程序食谱分类列表 */
     Route::post('/menu/list','MenuController@getList');
-
-    /** 用户充值vip */
-    Route::post('/charge/vip','ChargeController@rechargeVip');
-
-    /** 微信小程序支付回调 */
-    Route::post('/charge/weixin_callback','ChargeController@chargeCallback');
 
     /** 生成小程序码*/
     Route::post('/weixin/create_qrcode','WeixinController@createQrcode');
 
+
+    /** 公共接口部分 */
     Route::group(['prefix'=>'common'],function(){
+
+        /** 获取中国地址三级联动信息 */
         Route::get('/region','CommonController@getRegionInfo');
+    });
+
+    /** 用户模块接口 */
+    Route::group(['prefix'=>'user'],function(){
+
+        /** 获取微信用户openid */
+        Route::post('/get_openid','UserController@getOpenId');
+
+        /** 微信小程序一键登录获取手机号码 */
+        Route::post('/weixin_login','UserController@weixLogin');
+
+        /** vip卡列表 */
+        Route::get('/vip_card_list','UserController@vipCardList')->middleware('api.auth');
+    });
+
+
+    /** 支付下单相关接口 */
+    Route::group(['prefix'=>'charge'],function(){
+
+        /** 微信小程序支付回调 */
+        Route::post('/weixin_callback','ChargeController@chargeCallback');
+
+        /** 用户充值vip */
+        Route::post('/vip','ChargeController@rechargeVip')->middleware('api.auth');
     });
 
 });
